a15cc1c4c2bb7bc7f9b5d405f702555bed367958
Eder Taveira dos Santos <edertaveira@gmail.com>
2019-01-31 20:38:58 -0200

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,12 +4,12 @@ import 'firebase/auth';
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDO8tieZFRcn5Xbfh6CWetiykq6w5WH540",
-    authDomain: "shopping-cb306.firebaseapp.com",
-    databaseURL: "https://shopping-cb306.firebaseio.com",
-    projectId: "shopping-cb306",
-    storageBucket: "shopping-cb306.appspot.com",
-    messagingSenderId: "235621042783"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 firebase.initializeApp(config);
 //firebase.firestore().settings({ timestampsInSnapshots: true });
